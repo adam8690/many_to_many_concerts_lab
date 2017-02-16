b9f6daab4ce41ae0056fe8246ba00a98e97f655a
Chris <cbrown605@googlemail.com>
2017-02-16 14:07:37 +0000

    created tables in database, fixed typo in .sql file.

--- db/concert.sql
+++ db/concert.sql
@@ -12,12 +12,12 @@ location VARCHAR(255)
 CREATE TABLE artists(
 id SERIAL4 PRIMARY KEY,
 name VARCHAR(255),
-genre VARCHAR(255),
+genre VARCHAR(255)
 );
 
 CREATE TABLE gigs(
 id SERIAL4 PRIMARY KEY,
-venue_id  REFERENCES venues(id) ON DELETE CASCADE,
-artist_id REFERENCES artists(id) ON DELETE CASCADE,
-date DATE 
+venue_id INT4 REFERENCES venues(id) ON DELETE CASCADE,
+artist_id INT4 REFERENCES artists(id) ON DELETE CASCADE,
+date DATE
 );
